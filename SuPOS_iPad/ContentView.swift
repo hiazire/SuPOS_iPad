6b408429059317258cf319bd37baebac26536ccc
RabiSu <hiazire@gmail.com>
2026-04-15 08:23:00 +0800

    Initial Commit

---
+++ SuPOS_iPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuPOS_iPad
+//
+//  Created by 蘇甫瀚 on 2026/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuPOS_iPad/SuPOS_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuPOS_iPadApp.swift
+//  SuPOS_iPad
+//
+//  Created by 蘇甫瀚 on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct SuPOS_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
